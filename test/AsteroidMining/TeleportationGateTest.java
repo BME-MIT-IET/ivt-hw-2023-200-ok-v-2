ae576b2d372ed160ef86579d2738604f8e51ecc5
babalex16 <82571265+babalex16@users.noreply.github.com>
2023-05-18 21:00:47 +0200

    Asteroid and TeleportationGate tests
    Inhereted methods from the abstract class Place not yet tested.

---
+++ test/AsteroidMining/TeleportationGateTest.java
@@ -0,0 +1,35 @@
+package test.AsteroidMining;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import src.AsteroidMining.TeleportationGate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TeleportationGateTest {
+    private TeleportationGate gate1;
+    private TeleportationGate gate2;
+
+    @BeforeEach
+    void setUp() {
+        gate1 = new TeleportationGate();
+        gate2 = new TeleportationGate();
+    }
+
+    @Test
+    void getGateTest_NotSetUp() {
+        assertNull(gate1.getGate());
+    }
+
+    @Test
+    void setGateTest() {
+        gate1.setGate(gate2);
+        //one way connection gate1 to gate 2
+        assertEquals(gate2, gate1.getGate());
+        assertNull(gate2.getGate());
+
+        //two-way connected
+        gate2.setGate(gate1);
+        assertEquals(gate1, gate2.getGate());
+    }
+}
