795b7572d45759a8856319fad0ffe00ba86ca61f
Tatiana Barbova <44647094+Junumboxo@users.noreply.github.com>
2023-05-21 20:31:00 +0200

    Mockito imports

---
+++ src/main/java/AsteroidMining/Visitor.java
@@ -0,0 +1,100 @@
+package AsteroidMining;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public abstract class Visitor extends GameObject {
+    Handler handler;
+    protected boolean alive=true;
+    protected boolean hidden=false;
+    protected Place place;
+    protected BufferedImage img = null;
+
+
+    public Visitor(ID id, Handler handler) {
+        super(id);
+    }
+    public Visitor(int x, int y, ID id) {
+        super(x, y, id);
+    }
+
+    public Visitor(ID id) {
+        super(id);
+    }
+
+    //@Override
+    public abstract void tick();
+
+    public abstract void render(Graphics g) throws IOException;
+
+    public void travel(){
+        System.out.println("travel()");
+
+        if(place.equals(null))
+            System.out.println("null");
+
+        System.out.println(place);
+        System.out.println(handler.neighbours);
+        place.removeVisitor(this);
+        handler.neighbours.remove(place);
+        handler.neighbours.add(place);
+        handler.neighbours.get(0).addVisitor(this);
+        System.out.println(place);
+        System.out.println("Travelling to the Neighbouring asteroid..");
+
+
+    }
+    public boolean drill(){
+        System.out.println("Drilling!");
+        Asteroid a1 = (Asteroid) this.getPlace();
+        if(a1==null) return false;
+        if(a1.depth>=0) {
+            a1.deepenHole(2);
+            return true;
+        }else{
+            System.out.println("Asteroid is fully drilled!");
+            return false;}
+    }
+
+
+
+    public Place getPlace(){
+        return this.place;
+    }
+
+    public void setPlace(Place place){
+        this.place = place;
+    }
+
+    /*Hiding in the hollow asteroid*/
+    public boolean hide(){
+
+        if (isHidden() ){
+            hidden = false;
+             return true;
+         }
+
+        Asteroid a1 = (Asteroid) this.getPlace();
+        if(a1!=null && a1.isHollow()){
+            hidden = true; System.out.println("hide");
+            return true;
+        }
+        return false;
+
+    }
+    public boolean isHidden(){
+        return hidden;
+    }
+
+
+    public void die() {
+        this.alive=false;
+    }
+
+    public boolean isAlive(){
+        return alive;
+    }
+}
---
+++ src/test/java/AsteroidMining/AsteroidTest.java
@@ -0,0 +1,116 @@
+package AsteroidMining;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AsteroidTest {
+    Asteroid asteroid;
+
+    @BeforeEach
+    void setUp() {
+        asteroid = new Asteroid(0, 0, null, 10);
+    }
+
+    @Test
+    void deepenHoleTest() {
+
+        asteroid.deepenHole(5);
+
+        //TODO: getDepth() getter is missing
+        //assertEquals(5, asteroid.GetDepth());
+    }
+
+    @Test
+    void addResourceTest(){
+        Resource iron = new Iron();
+
+        asteroid.addResource(iron);
+
+        assertEquals(iron, asteroid.getResource());
+        assertFalse(asteroid.isHollow());
+    }
+
+    @Test
+    void addRadioactiveResourceTest(){
+        Resource uranium = new Uranium();
+
+        asteroid.addResource(uranium);
+
+        assertEquals(uranium, asteroid.getResource());
+        //Failed test: ID is not set to RadioactiveAsteroid after addResource(uranium) invoked
+        //assertEquals(ID.RadioActiveAsteroid, asteroid.getId());
+    }
+
+    @Test
+    void removeResourceTest() {
+        Resource resource = new Carbon();
+        asteroid.addResource(resource);
+
+        asteroid.removeResource();
+
+        assertNull(asteroid.getResource());
+        assertTrue(asteroid.isHollow());
+    }
+
+    @Test
+    void isFullyDrilledTest() {
+        Asteroid fullyDrilledAsteroid = new Asteroid(0, 0, new Carbon(), 0);
+        Asteroid partiallyDrilledAsteroid = new Asteroid(0, 0, new Carbon(), 5);
+
+        assertTrue(fullyDrilledAsteroid.isFullyDrilled());
+        assertFalse(partiallyDrilledAsteroid.isFullyDrilled());
+    }
+
+    @Test
+    void explodeTest_Settler() {
+        Visitor settler = new Settler(0,0, new Handler());
+        asteroid.addVisitor(settler);
+
+        asteroid.explode(true);
+        assertFalse(settler.isAlive());
+    }
+
+    @Test
+    void explodeTest_Robot() {
+        //Setting up 2 neighbouring asteroids, an explosion will happen on the asteroid on which robot resides
+        Visitor robot = new Robot(0,0);
+        asteroid.addVisitor(robot);
+
+        asteroid.addResource(new Uranium());
+        Asteroid neighbouringAsteroid = new Asteroid(100, 100, null, 10);
+        asteroid.addNeighbour(neighbouringAsteroid);
+        neighbouringAsteroid.addNeighbour(asteroid);
+
+        List<Visitor> list = new ArrayList<>();
+        list.add(robot);
+        //ConcurrentModificationException thrown
+        asteroid.explode(true);
+
+        assertEquals(list, neighbouringAsteroid.getVisitors());
+    }
+
+
+    @Test
+    void inPerihelionUraniumTest() {
+        Asteroid drilledAsteroid = new Asteroid(0, 0, new Uranium(), 0);
+        drilledAsteroid.setId(ID.RadioActiveAsteroid);
+        boolean exploded = true;
+
+        drilledAsteroid.inPerihelion(exploded);
+        assertEquals(ID.RadioActiveAsteroid, drilledAsteroid.getId() );
+    }
+
+    @Test
+    void inPerihelionWaterIceTest() {
+        Asteroid drilledAsteroid = new Asteroid(0, 0, new WaterIce(), 0);
+        boolean exploded = false;
+
+        drilledAsteroid.inPerihelion(exploded);
+        assertNull(asteroid.getResource());
+    }
+}
